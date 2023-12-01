ce9bf72572b97b28ab0ce7da9b8af45e86edfd36
CoderSid063 <sudhansupanda063@gmail.in>
2023-12-01 19:12:35 +0530

    New Button function added

--- Myntra-Clone/src/components/HomeItem.jsx
+++ Myntra-Clone/src/components/HomeItem.jsx
@@ -1,12 +1,23 @@
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { bagActions } from "../store/bagSlice";
+import { IoBagAdd } from "react-icons/io5";
+import { MdDelete } from "react-icons/md";
 
 const HomeItem = ({ item }) => {
   const dispatch = useDispatch();
 
+  const bagItems = useSelector((store) => store.bag);
+
+  const elementfound = bagItems.indexOf(item.id) >= 0;
+
   const handleAddToBag = () => {
     dispatch(bagActions.addToBag(item.id));
   };
+
+  // const handleRemoveFromBag = () => {
+  //   dispatch(bagActions.removeFromBag(item.id));
+  // };
+
   return (
     <div className="item-container">
       <img className="item-image" src={item.image} alt="item image" />
@@ -20,9 +31,21 @@ const HomeItem = ({ item }) => {
         <span className="original-price">Rs ${item.original_price}</span>
         <span className="discount">({item.discount_percentage}% OFF)</span>
       </div>
-      <button className="btn-add-bag" onClick={handleAddToBag}>
-        Add to Bag
-      </button>
+
+      {elementfound ? (
+        <button type="button" className="btn btn-danger btn-add-bag">
+          <MdDelete />
+          Remove
+        </button>
+      ) : (
+        <button
+          type="button"
+          className="btn btn-success btn-add-bag"
+          onClick={handleAddToBag}
+        >
+          <IoBagAdd /> Add to Bag
+        </button>
+      )}
     </div>
   );
 };
